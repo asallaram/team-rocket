c8b4662616ab53e3cb492cb99c639b532951fb85
EC2 Default User <ec2-user@ip-10-18-68-130.eu-west-1.compute.internal>
2026-08-20 15:09:11 +0000

    copy starter, test mvn, docker build

---
+++ starter/src/main/java/com/neueda/leap/Main.java
@@ -0,0 +1,8 @@
+package com.neueda.leap;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: replace Team-Rocket with your team's actual name
+        System.out.println("Hello world from Team-Rocket's Sprint 1 project skeleton");
+    }
+}
